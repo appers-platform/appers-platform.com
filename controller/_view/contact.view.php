e231487d51f21025934f60dbd86984c64c145504
Vlad <vlad.amardi@gmail.com>
2015-03-30 00:39:36 +0300

    page 'contact' has been updated

--- controller/_view/contact.view.php
+++ controller/_view/contact.view.php
@@ -1,4 +1,8 @@
 <div class="jumbotron">
-	<h1>Глубокая бета...</h1>
-	<p class="lead"><a href="mailto:appers.platform@gmail.com">appers.platform@gmail.com</p>
+	<h1>Глубокая бета... *</h1>
+	<p class="lead">
+		<a href="https://github.com/appers-platform/">GitHub</a><br>
+		<a href="mailto:appers.platform@gmail.com">appers.platform@gmail.com</a><br>
+		* - код в любой момент может стать обратно несовместимым.
+	</p>
 </div>
