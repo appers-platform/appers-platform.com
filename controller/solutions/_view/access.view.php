20f95344064795ef4030c70657e92417f5308734
Vlad <vlad.amardi@gmail.com>
2015-04-20 16:55:26 +0300

    Description for solution 'access'

--- controller/solutions/_view/access.view.php
+++ controller/solutions/_view/access.view.php
@@ -12,3 +12,18 @@
 	]
 ],
 ")?>
+
+Управление с помощью терминала:
+<?solutions\highlighter::out("./exec ::access::admin
+[8965 2015.04.20 16:53:29]:	 Task \"::access::admin\" was launched.
+[8965 2015.04.20 16:53:29]:	 
+
+Options:
+	list [access=type] [limit=50] - list all users with access (limit 50 by default)
+	show user_id=000
+	edit user_id=000 [add=type[,type2]]
+	edit user_id=000 [delete=type[,type2]]
+
+
+[8965 2015.04.20 16:53:29]:	 Task \"::access::admin\" was finished. Time: 0.0003 s.
+")?>
