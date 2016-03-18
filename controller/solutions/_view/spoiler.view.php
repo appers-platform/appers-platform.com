caecd5bdeba3891ed7472f39fb6ee9ddc472604c
Vlad <vlad.amardi@gmail.com>
2016-03-18 19:16:58 +0200

    Solution 'spoiler'

---
+++ controller/solutions/_view/spoiler.view.php
@@ -0,0 +1,9 @@
+Пример:
+<?\solutions\spoiler::start('Lorem ipsum')?>
+Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, explicabo. Nemo enim ipsam voluptatem, quia voluptas sit.
+<?\solutions\spoiler::end()?>
+
+Исходный код:
+<?solutions\highlighter::out('<?\solutions\spoiler::start(\'Lorem ipsum\')?>
+Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, explicabo. Nemo enim ipsam voluptatem, quia voluptas sit.
+<?\solutions\spoiler::end()?>')?>
